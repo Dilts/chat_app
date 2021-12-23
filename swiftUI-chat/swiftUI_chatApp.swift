0d1f921c1e969af0c72bd3f629d630ef8d388803
Brian Dilts <brianedilts@gmail.com>
2021-12-23 11:24:20 -0700

    Initial Commit

---
+++ swiftUI-chat/swiftUI_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_chatApp.swift
+//  swiftUI-chat
+//
+//  Created by Brian Dilts on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
