9176c100d7206106c40da1562188cdeda3299819
Brian Dilts <brianedilts@gmail.com>
2021-12-23 12:02:46 -0700

    - set up folder structure for MVVM - add color sets - add image set - rename ContentView to HomeView

--- swiftUI-chat/Views/HomeView.swift
+++ swiftUI-chat/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  swiftUI-chat
 //
 //  Created by Brian Dilts on 12/23/21.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
